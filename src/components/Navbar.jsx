37c9a8ddcdbb95a52493aaa305145a547b003a7b
adithyab11 <adithyabs11@gmail.com>
2024-06-07 15:01:56 +0530

    successfully registered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Student App</a>
+    <Link class="navbar-brand" to="/">Student App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/Search">SearchStudent</a>
-        <a class="nav-link" href="/delete">DeleteStudent</a>
-        <a class="nav-link" href="/viewall">View All Student</a>
+        <Link class="nav-link active" aria-current="page" to="/Search">SearchStudent</Link>
+        <Link class="nav-link" to="/delete">DeleteStudent</Link>
+        <Link class="nav-link" to="/viewall">View All Student</Link>
         
       </div>
     </div>
